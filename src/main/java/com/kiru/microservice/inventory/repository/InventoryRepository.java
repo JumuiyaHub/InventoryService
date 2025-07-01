daa5df01603df2c8feb6d1820a6255777ca0e54a
KiprutoKelvin <127014004+KiprutoKelvin@users.noreply.github.com>
2025-07-01 08:29:33 +0300

    - Extended JpaRepository for Inventory entity - Implemented custom method: existsBySkuCodeAndQuantityIsGreaterThanEqual - Enables querying inventory by SKU code and minimum quantity

---
+++ src/main/java/com/kiru/microservice/inventory/repository/InventoryRepository.java
@@ -0,0 +1,8 @@
+package com.kiru.microservice.inventory.repository;
+
+import com.kiru.microservice.inventory.model.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+    boolean existsBySkuCodeAndQuantityIsGreaterThanEqual(String skuCode, Integer quantity);
+}
